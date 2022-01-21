82ef21f0ee0f205e1c8ea4959b0d3aaa636a695a
DAMIAN SORTINO <damiansortino@gmail.com>
2022-01-21 12:39:12 -0300

    Update frmStock.Designer.cs

--- Design Dashboard Modern/Vistas/frmStock.Designer.cs
+++ Design Dashboard Modern/Vistas/frmStock.Designer.cs
@@ -61,10 +61,10 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             // tbFiltro
             // 
-            this.tbFiltro.Font = new System.Drawing.Font("Ubuntu Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbFiltro.Location = new System.Drawing.Point(12, 33);
             this.tbFiltro.Name = "tbFiltro";
-            this.tbFiltro.Size = new System.Drawing.Size(344, 25);
+            this.tbFiltro.Size = new System.Drawing.Size(344, 24);
             this.tbFiltro.TabIndex = 1;
             this.tbFiltro.Text = "Código del Producto";
             this.tbFiltro.Enter += new System.EventHandler(this.tbFiltro_Enter);
@@ -73,7 +73,7 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             this.btnStockxProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(207)))), ((int)(((byte)(77)))));
             this.btnStockxProducto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStockxProducto.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStockxProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStockxProducto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnStockxProducto.Location = new System.Drawing.Point(12, 74);
             this.btnStockxProducto.Name = "btnStockxProducto";
@@ -87,9 +87,8 @@ namespace Design_Dashboard_Modern.Vistas
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(207)))), ((int)(((byte)(77)))));
-            this.button3.Enabled = false;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button3.Location = new System.Drawing.Point(220, 74);
             this.button3.Name = "button3";
@@ -133,16 +132,16 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             // tbCodigoProductoMovStock
             // 
-            this.tbCodigoProductoMovStock.Font = new System.Drawing.Font("Ubuntu Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCodigoProductoMovStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbCodigoProductoMovStock.Location = new System.Drawing.Point(99, 101);
             this.tbCodigoProductoMovStock.Name = "tbCodigoProductoMovStock";
-            this.tbCodigoProductoMovStock.Size = new System.Drawing.Size(232, 25);
+            this.tbCodigoProductoMovStock.Size = new System.Drawing.Size(232, 24);
             this.tbCodigoProductoMovStock.TabIndex = 14;
             this.tbCodigoProductoMovStock.Text = "Código del Producto";
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.Location = new System.Drawing.Point(176, 206);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
@@ -154,7 +153,7 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(264, 206);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
@@ -168,7 +167,7 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(167)))), ((int)(((byte)(167)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button2.Location = new System.Drawing.Point(234, 280);
             this.button2.Name = "button2";
@@ -183,7 +182,7 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(185)))), ((int)(((byte)(55)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button1.Location = new System.Drawing.Point(43, 280);
             this.button1.Name = "button1";
@@ -195,7 +194,7 @@ namespace Design_Dashboard_Modern.Vistas
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Ubuntu Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(214, 211);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(43, 22);
@@ -204,20 +203,20 @@ namespace Design_Dashboard_Modern.Vistas
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Ubuntu Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(84, 215);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 22);
+            this.label3.Size = new System.Drawing.Size(81, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Cantidad";
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Ubuntu Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton2.Location = new System.Drawing.Point(215, 145);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(60, 24);
+            this.radioButton2.Size = new System.Drawing.Size(59, 22);
             this.radioButton2.TabIndex = 7;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Sale";
@@ -226,10 +225,10 @@ namespace Design_Dashboard_Modern.Vistas
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Ubuntu Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton1.Location = new System.Drawing.Point(120, 145);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(70, 24);
+            this.radioButton1.Size = new System.Drawing.Size(66, 22);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Entra";
@@ -238,20 +237,20 @@ namespace Design_Dashboard_Modern.Vistas
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Ubuntu", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(20, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.Size = new System.Drawing.Size(156, 18);
             this.label2.TabIndex = 5;
             this.label2.Text = "Tipo de Movimiento";
             // 
             // cboxTipoMovStock
             // 
-            this.cboxTipoMovStock.Font = new System.Drawing.Font("Ubuntu Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboxTipoMovStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboxTipoMovStock.FormattingEnabled = true;
             this.cboxTipoMovStock.Location = new System.Drawing.Point(181, 23);
             this.cboxTipoMovStock.Name = "cboxTipoMovStock";
-            this.cboxTipoMovStock.Size = new System.Drawing.Size(183, 28);
+            this.cboxTipoMovStock.Size = new System.Drawing.Size(183, 26);
             this.cboxTipoMovStock.TabIndex = 4;
             // 
             // frmStock
